1675b3adf11c64137b957b8c04a06873b0fc7167
Joe Reynolds <joereynolds952@gmail.com>
2019-07-27 22:41:44 +0100

    Publish meteor article

--- assets/articles/meteor/code/normalise.py
+++ assets/articles/meteor/code/normalise.py
@@ -18,12 +18,4 @@ def normalise_array(numbers, min_range, max_range):
 def normalise(n, min_n, max_n, min_range, max_range):
     return (n - min_n) * (max_range - min_range) / (max_n - min_n) + min_range
 
-
-meteors_notes = open('./meteors-1980-notes.csv')
-numbers = [float(line.replace('\n', '')) for line in meteors_notes]
-normalised = normalise_array(numbers, 20, 20000)
-
-with open('../data/meteors-1980-notes-normalised.csv', 'w') as writer:
-    for number in normalised:
-        writer.write(str(int(number)) + "\n")
-
+print(normalise_array([37.1, 15, 24.8, 34.8, 31.25], 20, 20000))
